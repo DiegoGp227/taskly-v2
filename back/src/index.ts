2cd46e260c0b38abf09c2e2ce4c8307d0f3a3275
DiegoGp227 <diego.gp227@gmail.com>
2025-11-02 01:51:51 -0500

    [ test ] test

--- back/src/index.ts
+++ back/src/index.ts
@@ -7,17 +7,8 @@ const PORT = process.env.PORT ? Number(process.env.PORT) : 4000;
 const app = new Elysia()
   .use(
     cors({
-      origin: [
-        "http://localhost:3000",
-        "http://localhost:5000",
-        "http://192.168.0.3:3000",
-        "http://192.168.0.3:5000",
-        "https://taskly.devdiego.work",
-        "https://api.devdiego.work",
-      ],
+      origin: true,
       credentials: true,
-      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-      allowedHeaders: ["Content-Type", "Authorization"],
     })
   )
   .use(router)
